d81058cc15cc9638f144a84a83e581625eafea4e
Adrian Lodeiro <alodeiro.pro@gmail.com>
2022-09-10 13:56:56 +0200

    Tests setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFiles: ['./jest.setup.js'],
+    transformIgnorePatterns: [],
+    
+    // ModuleNameMapper only if we import CSS on our components for testing
+    // moduleNameMapper: {
+    //     '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+    // },
+}
