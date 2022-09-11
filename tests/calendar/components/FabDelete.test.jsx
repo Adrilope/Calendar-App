813493d17541bf1990771fae1f80db20fc281bea
Adrian Lodeiro <alodeiro.pro@gmail.com>
2022-09-11 19:25:40 +0200

    FabDelete tests

--- tests/calendar/components/FabDelete.test.jsx
+++ tests/calendar/components/FabDelete.test.jsx
@@ -1,16 +1,52 @@
-import { render, screen } from "@testing-library/react"
-import { Provider } from "react-redux"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { FabDelete } from "../../../src/calendar/components/FabDelete"
-import { store } from "../../../src/store"
+import { useCalendarStore } from "../../../src/hooks/useCalendarStore"
+
+
+jest.mock('../../../src/hooks/useCalendarStore')
 
 
 describe('Tests with <FabDelete />', () => { 
-    test('should render correctly', () => { 
-        render(
-            <Provider store={store}>
-                <FabDelete />
-            </Provider>
-        )
-        screen.debug()
+    const mockStartDeletingEvent = jest.fn()
+
+    beforeEach(() => jest.clearAllMocks())
+
+
+    test('should render correctly', () => {
+        useCalendarStore.mockReturnValue({
+            hasEventSelected: false
+        })
+
+        render(<FabDelete />)
+        const btn = screen.getByLabelText('btn-delete')
+
+        expect(btn.classList).toContain('btn')
+        expect(btn.classList).toContain('btn-danger')
+        expect(btn.classList).toContain('fab-danger')
+        expect(btn.style.display).toBe('none')
+    })
+
+    test('should show the button if hasEventSelected is true', () => { 
+        useCalendarStore.mockReturnValue({
+            hasEventSelected: true
+        })
+
+        render(<FabDelete />)
+        const btn = screen.getByLabelText('btn-delete')
+
+        expect(btn.style.display).toBe('')
+    })
+
+    test('should call startDeletingEvent if hasEventSelected is true', () => { 
+        useCalendarStore.mockReturnValue({
+            startDeletingEvent: mockStartDeletingEvent,
+            hasEventSelected: true
+        })
+
+        render(<FabDelete />)
+        const btn = screen.getByLabelText('btn-delete')
+        fireEvent.click(btn)
+
+        expect(mockStartDeletingEvent).toHaveBeenCalled()
     })
 })
